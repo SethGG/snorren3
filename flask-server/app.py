f5139e0597c67cf6480edc21ec0add52fb9255e5
dzee <df.zee@hotmail.com>
2021-08-27 14:20:02 +0200

    first commit

---
+++ flask-server/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__,
+            template_folder='../react-app/build',
+            static_folder='../react-app/build/static')
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+app.run(debug=True)
