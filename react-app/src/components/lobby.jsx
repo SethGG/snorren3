eb3596b4029b215efce1c53a55ee04013673f432
dzee <df.zee@hotmail.com>
2022-02-27 22:13:42 +0100

    before reinstall

---
+++ react-app/src/components/lobby.jsx
@@ -0,0 +1,15 @@
+const Lobby = () => {
+  return (
+    <section className="section is-large">
+      <div className="container is-max-mobile">
+        <div className="card">
+          <div className="card-content">
+            Lobby test
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Lobby
